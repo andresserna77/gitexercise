b9167ee80298ff1ac28487f3a6f25b6e10feaa2f
andres.serna <andresf.serna@gmail.com>
2021-11-02 14:45:16 -0500

    Adding the sum and substract Methods

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,11 @@
 public class Calculator {
-    
+    public double sum(double op1, double op2){
+        double result = op1 + op2;
+        return result;
+    }
+
+    public double substract(double op1, double op2){
+        double result = op1 - op2;
+        return result;
+    }
 }
