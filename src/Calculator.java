748ff6d120262a573f454a108185016e8c131861
andres.serna <andresf.serna@gmail.com>
2021-11-03 09:26:01 -0500

    Adding Multiplication and Division Methods

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,5 @@
 public class Calculator {
+<<<<<<< HEAD
     public double sum(double op1, double op2){
         double result = op1 + op2;
         return result;
@@ -8,4 +9,16 @@ public class Calculator {
         double result = op1 - op2;
         return result;
     }
+=======
+    public double multiplication(double op1, double op2){
+        double result = op1 * op2;
+        return result;
+    }
+
+    public double division(double op1, double op2){
+        double result = op1 / op2;
+        return result;
+    }
+    
+>>>>>>> 61c3d19 (Adding Multiplication and Division Methods)
 }
