43b420a43820657096fcc99985a81aaacb0783ca
andres.serna <andresf.serna@gmail.com>
2021-11-03 09:30:13 -0500

    all methods are included in advanced_calculator with rebase command

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,5 @@
 public class Calculator {
-<<<<<<< HEAD
-    public double sum(double op1, double op2){
+   public double sum(double op1, double op2){
         double result = op1 + op2;
         return result;
     }
@@ -9,7 +8,7 @@ public class Calculator {
         double result = op1 - op2;
         return result;
     }
-=======
+
     public double multiplication(double op1, double op2){
         double result = op1 * op2;
         return result;
@@ -20,5 +19,5 @@ public class Calculator {
         return result;
     }
     
->>>>>>> 61c3d19 (Adding Multiplication and Division Methods)
+
 }
